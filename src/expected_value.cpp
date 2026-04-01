04ce11e3764f79e6931e12d1363560b52e6d4559
Gabriel Carmona <191816876+Gcarmnonapy7@users.noreply.github.com>
2026-04-01 00:51:35 -0300

    Fixing : expected value cpp

--- src/expected_value.cpp
+++ src/expected_value.cpp
@@ -1,15 +1,21 @@
 #include "expected_value.hpp"
 #include <stdexcept>
 
+double ExpectedValue::calculate(
+    const std::vector<double>& values,
+    const std::vector<double>& probabilities,
+    std::function<double(double)> f)
+{
 
-double ExpectedValue::calculate(const std::vector<double>& values, const std::vector<double>& probabilities, std::function<double (double)> f){
-    if (values.size() != probabilities.size()) {
-        throw std::invalid_argument("Values and probabilities must have the same size");
-    }
+    if(values.size() != probabilities.size())
+        throw std::invalid_argument(
+            "Values and probabilities must have the same size"
+        );
 
     double expected_value = 0.0;
-    for (size_t i = 0; i < values.size(); ++i) {
+
+    for(size_t i = 0; i < values.size(); ++i)
         expected_value += f(values[i]) * probabilities[i];
-    }
+
     return expected_value;
-};
+}
